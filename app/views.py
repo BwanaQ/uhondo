b79b873d31262dd73bb731c77e5657ac6382f9e7
BwanaQ <thunjawax@gmail.com>
2021-02-20 15:31:22 +0300

    add url to list of sources

--- app/views.py
+++ app/views.py
@@ -1,6 +1,6 @@
 from flask import render_template
 from app import app
-from .request import get_articles
+from .request import get_articles, get_sources
 # Views
 
 
@@ -10,7 +10,8 @@ def index():
     View root page function that returns the index page and its data
     '''
     # getting us news
-    us_articles = get_articles('us')
-    print(us_articles)
+    #us_articles = get_articles('us')
+    # print(us_articles)
+    all_sources = get_sources()
     title = 'Uhondo | Home of the latest News'
-    return render_template('index.html', title=title, us=us_articles)
+    return render_template('index.html', title=title, sources=all_sources)
