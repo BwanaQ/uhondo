0fc3637112471f2c08957c60e3d662d934f786dd
BwanaQ <thunjawax@gmail.com>
2021-02-23 08:28:01 +0300

    clean up views.py

--- app/views.py
+++ app/views.py
@@ -9,9 +9,6 @@ def index():
     '''
     View root page function that returns the index page and its data
     '''
-    # getting us news
-    #us_articles = get_articles('us')
-    # print(us_articles)
     all_sources = get_sources()
     title = 'Uhondo | Home of the latest News'
     return render_template('index.html', title=title, sources=all_sources)
@@ -23,6 +20,5 @@ def source(id):
     View aticles from one source function
     '''
     source = get_articles(id)
-    #title = f'{article.name}'
 
     return render_template('source.html',  source=source)
